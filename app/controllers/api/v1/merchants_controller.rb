7011c087c40168e261fa15d444de0a21874e059c
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-04-29 20:12:55 -0600

    establishes all merchant endpoints

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,31 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
+    render json: Merchant.all
+  end
 
+  def show
+    render json: Merchant.find(params[:id])
+  end
+
+  def create
+    render json: Merchant.create(create_params)
+  end
+
+  def update
+    render json: Merchant.update(update_params)
+  end
+
+  def destroy
+    render json: Merchant.destroy(params[:id])
+  end
+
+  private
+
+  def create_params
+    params.require(:merchant).permit(:name)
+  end
+
+  def update_params
+    params.require(:merchant).permit(:name)
   end
 end
--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -7,5 +7,55 @@ describe 'Merchants API' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(5)
+  end
+
+  it "can show one merchant" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it "can create a merchant" do
+    merchant_params = {name: "Mike Dao"}
+
+    post "/api/v1/merchants", params: {merchant: merchant_params}
+    merchant = Merchant.last
+
+    expect(response).to be_successful
+    expect(merchant.name).to eq(merchant_params[:name])
+  end
+
+  it "can update a merchant" do
+    id = create(:merchant).id
+    previous_name = Merchant.last.name
+    merchant_params = {name: "Mike Dao"}
+
+    put "/api/v1/merchants/#{id}", params: {merchant: merchant_params}
+    merchant = Merchant.find(id)
+
+    expect(response).to be_successful
+    expect(merchant.name).to_not eq(previous_name)
+    expect(merchant.name).to eq("Mike Dao")
+  end
+
+  it "can delete a merchant" do
+    merchant = create(:merchant)
+
+    expect(Merchant.count).to eq(1)
+
+    delete "/api/v1/merchants/#{merchant.id}"
+
+    expect(response).to be_successful
+    expect(Merchant.count).to eq(0)
+    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
