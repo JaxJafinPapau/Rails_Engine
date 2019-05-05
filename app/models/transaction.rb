4dcd480ae785e41ac72edbcc0ac9a2732dc5dde8
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-05 12:03:02 -0700

    creates find and find_all for transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,40 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  def self.find_all(params)
+    if params[:id]
+      all.where(id: params[:id])
+    elsif params[:invoice_id] && !params[:id]
+      all.where(invoice_id: params[:invoice_id])
+    elsif params[:credit_card_number] && !params[:id]
+      all.where(credit_card_number: params[:credit_card_number])
+    elsif params[:credit_card_expiration_date] && !params[:id]
+      all.where(credit_card_expiration_date: params[:credit_card_expiration_date])
+    elsif params[:result] && !params[:id]
+      all.where(result: params[:result])
+    elsif params[:created_at] && !params[:id]
+      all.where(created_at: params[:created_at])
+    elsif params[:updated_at] && !params[:id]
+      all.where(updated_at: params[:updated_at])
+    else
+      all
+    end
+  end
+
+  def self.search(params)
+    if params[:id]
+      find(params[:id])
+    elsif params[:invoice_id] && !params[:id]
+      find_by(invoice_id: params[:invoice_id])
+    elsif params[:credit_card_number] && !params[:id]
+      find_by(credit_card_number: params[:credit_card_number])
+    elsif params[:credit_card_expiration_date] && !params[:id]
+      find_by(credit_card_expiration_date: params[:credit_card_expiration_date])
+    elsif params[:result] && !params[:id]
+      find_by(result: params[:result])
+    elsif params[:created_at] && !params[:id]
+      find_by(created_at: params[:created_at])
+    elsif params[:updated_at] && !params[:id]
+      find_by(updated_at: params[:updated_at])
+    end
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "find_all" do
+    it "should give all transactions with no params" do
+      merch_id = create(:merchant).id
+      cust_id = create(:customer).id
+      inv_id = create(:invoice, merchant_id: merch_id, customer_id: cust_id).id
+      create_list(:transaction, 5, invoice_id: inv_id)
+
+      params = {}
+
+      expect(Transaction.find_all(params).count).to eq(5)
+    end
+  end
+
+  #write tests for the full functionality of these methods
+
+  describe "search" do
+    it "should return the first transaction with matching id" do
+      transaction = create(:transaction)
+
+      params = {id: transaction.id}
+
+      expect(Customer.search(params).id).to eq(transaction.id)
+    end
+  end
 end
